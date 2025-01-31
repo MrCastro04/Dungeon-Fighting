2cca06a3fd9ede4dd7ef068f16de6536fe206277
MrCastro04 <slavamora684@gmail.com>
2025-01-31 14:31:57 +0200

    малі оновлення в AI контролері
    додав самі базові речі . В старті заходимо в стейт, і оновленя коден кадр , + публічний метод SwitchState

--- Dungeon Adventures/Assets/Scripts/Character/EnemyController.cs
+++ Dungeon Adventures/Assets/Scripts/Character/EnemyController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Character
@@ -7,11 +8,29 @@ namespace Character
         [SerializeField] private float _chaseRange;
 
         private AIBaseState _currentState;
+        private AIReturnState _returnState = new();
         private AIChaseState _chaseState = new ();
 
         private void Awake()
         {
+            _currentState = _returnState;
+        }
 
+        private void Start()
+        {
+            _currentState.EnterState(this);
+        }
+
+        private void Update()
+        {
+            _currentState.UpdateState(this);
+        }
+
+        public void SwitchState(AIBaseState newState)
+        {
+            _currentState = newState;
+
+            _currentState.EnterState(this);
         }
     }
 }
