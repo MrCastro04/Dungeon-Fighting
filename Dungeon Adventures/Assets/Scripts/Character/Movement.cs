243f9b22f7cfd3ed93703be8860311be96ebb809
MrCastro04 <slavamora684@gmail.com>
2025-01-31 14:42:02 +0200

    оновив Movement
    додав переміщення до цілі, перевірку чи досяг агент цілі, і звичайне переміщення по offset-у

--- Dungeon Adventures/Assets/Scripts/Character/Movement.cs
+++ Dungeon Adventures/Assets/Scripts/Character/Movement.cs
@@ -28,6 +28,37 @@ namespace Character
             _moveVector = new Vector3(input.x, 0f, input.y);
         }
 
+        public void MoveAgentByOffset(Vector3 offset)
+        {
+            _agent.Move(offset);
+        }
+
+        public void MoveAgentByDestination(Vector3 destination)
+        {
+            _agent.SetDestination(destination);
+        }
+
+        public bool ReachedDestination()
+        {
+            if (_agent.pathPending)
+            {
+                return false;
+            }
+
+            if (_agent.remainingDistance > _agent.stoppingDistance)
+            {
+                return false;
+            }
+
+            if (_agent.hasPath || _agent.velocity.sqrMagnitude != 0f)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+
         private void MovePlayer()
         {
             var offset = _moveVector * (Time.deltaTime * _agent.speed);
