a4dc854ab4427f07af557fb5016c94a119f0afb8
MrCastro04 <slavamora684@gmail.com>
2025-01-31 14:28:35 +0200

    додав пустий скрипт ReturnState

---
+++ Dungeon Adventures/Assets/Scripts/Character/AIReturnState.cs
@@ -0,0 +1,15 @@
+namespace Character
+{
+    public class AIReturnState : AIBaseState
+    {
+        public override void EnterState(EnemyController controller)
+        {
+
+        }
+
+        public override void UpdateState(EnemyController controller)
+        {
+
+        }
+    }
+}
--- Dungeon Adventures/Assets/Scripts/Character/EnemyController.cs
+++ Dungeon Adventures/Assets/Scripts/Character/EnemyController.cs
@@ -1,12 +1,18 @@
+using System;
 using UnityEngine;
 
 namespace Character
 {
     public class EnemyController : MonoBehaviour
     {
+        [SerializeField] private float _chaseRange;
+
         private AIBaseState _currentState;
         private AIChaseState _chaseState = new ();
 
-        
+        private void Awake()
+        {
+         
+        }
     }
 }
