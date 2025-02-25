94c01d5dc368b11de50eda1832bb1241a8d45808
MrCastro04 <slavamora684@gmail.com>
2025-02-25 16:04:50 +0200

    Буду переходити на UIToolkit. Створив UIBaseState

---
+++ Dungeon Adventures/Assets/UIBaseState.cs
@@ -0,0 +1,16 @@
+namespace DefaultNamespace
+{
+    public abstract class UIBaseState
+    {
+        public UIController ControllerCmp;
+
+        public UIBaseState( UIController controller )
+        {
+            ControllerCmp = controller;
+        }
+
+        public abstract void EnterState();
+
+        public abstract void SelectButton();
+    }
+}
